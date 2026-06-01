1c96c9567e6b2cbb64edf8084b87e52077e30509
StanislavRudyk <stasikrudik63@gmail.com>
2026-06-01 20:13:34 +0300

    настроил подключение к бд и создал таблицу user

---
+++ backend/modeles/database.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+PG_USER = os.getenv("POSTGRES_USER")
+PG_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+PG_HOST = os.getenv("POSTGRES_HOST")
+PG_DB = os.getenv("POSTGRES_DB")
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DB}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
--- run.py
+++ run.py
@@ -4,7 +4,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
 import redis
+from backend.modeles.database import engine, Base
+from backend.modeles import models
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Agronomist API")
 
 
